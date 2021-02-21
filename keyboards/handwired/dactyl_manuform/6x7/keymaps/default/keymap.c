1474ff23b797c699b95e225996227fa83f8aaa39
Devasia Thomas <iamdevasia@gmail.com>
2021-02-20 16:04:34 -0800

    Update keymap.c
    Switched to colemak

--- keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
@@ -1,40 +1,51 @@
 #include QMK_KEYBOARD_H
 
-#define RAISE MO(2)
-#define LOWER MO(1)
+#define RAISE MO(1)
+#define LOWER MO(2)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-     KC_HOME, KC_F1  , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,  KC_HOME,
-     KC_HOME, KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,  KC_HOME,
-     KC_HOME, KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,  KC_HOME,
-     KC_HOME, KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,  KC_HOME,
-     KC_HOME, KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,KC_HOME,
-                               KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                               RAISE,  KC_SPC,                        KC_ENT,  LOWER,
-                                               KC_TAB, KC_HOME,                       KC_END,  KC_DEL,
-                                               KC_BSPC,KC_GRV,                        KC_LGUI, KC_LALT
+     KC_ESC,   KC_F1  , KC_F2 , KC_F3 , KC_F4, KC_F5 , KC_F6  ,                         KC_F7 , KC_F8 , KC_F9  , KC_F10, KC_F11 , KC_F12 ,  M(0),
+     KC_GRAVE, KC_1   , KC_2  , KC_3  , KC_4 , KC_5  , KC_6   ,                         KC_7  , KC_8  , KC_9   , KC_0  , KC_0   , KC_MINS,  KC_EQL,
+     KC_INS,   KC_TAB , KC_Q  , KC_W  , KC_F , KC_P  , KC_G   ,                         KC_J  , KC_L  , KC_U   , KC_Y  , KC_SCLN, KC_LBRC,  KC_RBRC,
+     KC_LALT,  KC_CAPS, KC_A  , KC_R  , KC_S , KC_T  , KC_D   ,                         KC_H  , KC_N  , KC_E   , KC_I  , KC_O   , KC_QUOT,  KC_BSLS,
+     KC_LCTL,  KC_LSFT, KC_Z  , KC_X  , KC_C , KC_V  , KC_B   ,                         KC_K  , KC_M  , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,  KC_ALGR,
+                                    KC_LEFT,KC_UP,                                                  KC_DOWN, KC_RGHT,
+                                               KC_ENT,  KC_DEL,                        KC_BSPC,  KC_SPC,
+                                               KC_HOME, KC_LGUI,                       KC_RGUI,  KC_END,
+                                               KC_PGUP, RAISE,                         LOWER, KC_PGDOWN  
   ),
   [1] = LAYOUT(
-     KC_HOME, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6  ,                        KC_F7 ,  KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,KC_HOME, 
-     KC_HOME, KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL, KC_HOME, 
-     KC_HOME, _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,KC_HOME, 
-     KC_HOME, _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,KC_HOME, 
-     KC_HOME, _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,KC_HOME, 
-                                RESET,KC_PSCR,                                                         _______,KC_P0,
-                                             _______,_______,            _______,_______,
+     _______, _______,_______,_______,_______,KC_BRIU,KC_BRID,                        KC_VOLU,KC_VOLD,KC_MPRV,KC_MPLY,KC_MNXT,_______,RESET , 
+     _______, _______,_______,_______,_______,_______,_______,                        KC_LPRN, KC_P7 , KC_P8 , KC_P9 ,KC_PSLS,_______,_______, 
+     _______, _______,_______,_______,_______,_______,_______,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_PAST,_______,_______, 
+     _______, _______,_______,_______,_______,_______,_______,                        KC_CIRC, KC_P1 , KC_P2 , KC_P3 ,KC_PPLS,_______,_______, 
+     _______, _______,_______,_______,_______,_______,_______,                        KC_PERC,_______, KC_P0 ,KC_PDOT,KC_PMNS,_______,KC_PSCR, 
+                              _______,_______,                                                         _______,_______,
+                                             KC_PENT,_______,            _______,_______,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______
   ),
   [2] = LAYOUT(
-      KC_HOME,KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_HOME, 
-      KC_HOME,_______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,KC_HOME, 
-      KC_HOME,_______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,KC_HOME, 
-      KC_HOME,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,KC_HOME, 
-      KC_HOME,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,KC_HOME, 
-                                RESET,_______,                                                        KC_EQL ,_______,
+      _______,_______,_______,_______,_______,KC_BRIU,KC_BRID,                        KC_VOLU,KC_VOLD,KC_MPRV,KC_MPLY,KC_MNXT,_______,RESET , 
+      _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,_______, 
+      _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,_______, 
+      _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,_______, 
+      _______,_______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,KC_PSCR, 
+                              _______,_______,                                                        _______ ,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
   ),
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch(id) {
+            //Lock Screen    
+            case 0:
+                return MACRO(D(LGUI), T(L), U(LGUI), END);
+        }
+    }
+    return MACRO_NONE;
 };
